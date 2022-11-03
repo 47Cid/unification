5d080ff598b9b667ab5c39047b3afd860af79e5e
sid <siddhantkalgutkar@gmail.com>
2022-11-03 21:45:07 +0530

    minor refactoring

--- src/Unification.hs
+++ src/Unification.hs
@@ -1,23 +1,25 @@
-module Lib
+module Unification
     ( run
     ) where
 import qualified Data.Map    as M
 import           Data.Maybe  (fromMaybe)
 
 run :: IO ()
-run = print $ unify (Application "f" [Variable "X", Application "h" [Variable "X"], Variable "Y", Application "g" [Variable "Y"]])
-                    (Application "f" [Application "g" [Variable "Z"], Variable "W", Variable "Z", Variable "X"]) (Binding M.empty)
+-- run = print $ unify (Application "f" [Variable "X", Application "h" [Variable "X"], Variable "Y", Application "g" [Variable "Y"]])
+                    -- (Application "f" [Application "g" [Variable "Z"], Variable "W", Variable "Z", Variable "X"]) (Binding M.empty)
 -- run = print $ unify (Application "f" [Atom "a", Atom "b" , Application "bar" [Atom "t"]])
 --                     (Application "f" [Atom "a",  Variable "V", Variable "X"]) (Binding M.empty)
 -- run = print $ unify (Application "f" [Atom "a", Variable "V" , Application "bar" [Variable "D"]])
 --                     (Application "f" [Variable "D",  Atom "k", Application "bar" [Atom "a"]]) (Binding M.empty)
 -- run = print $ unify (Application "f" [Variable "X", Variable "Y"])
 --                     (Application "f" [Atom "r", Application "g" [Variable "X"]]) (Binding M.empty)
+run = print $ unify (Variable "X")
+                    (Application "f" [Variable "X"]) (Binding M.empty)
 
 data ProgVal = Atom String
              | Variable String
              | Application String [ProgVal]
-      deriving(Show, Read ,Eq , Ord)
+      deriving(Show, Eq, Ord)
 
 data Binding  =   Binding (M.Map ProgVal ProgVal) | Error String
       deriving(Show, Eq)
@@ -26,7 +28,6 @@ unify :: ProgVal -> ProgVal -> Binding -> Either Binding String
 unify _ _ (Error err)          =  Right err
 unify (Variable x) y bindings  =  unifyVariable (Variable x) y bindings
 unify  x (Variable y) bindings =  unifyVariable (Variable y) x bindings
-
 unify (Application _ [x]) (Application _ [y]) bindings = unify x y bindings
 unify (Application xname (x:xs)) (Application yname (y:ys)) bindings = unify (Application xname xs) (Application yname ys)
                                                  $ either id (\_ -> Error "Application Fail") (unify x y bindings)
@@ -45,11 +46,13 @@ unifyVariable y x bindings
                             | y /= x = extendBindings y x bindings
                             | otherwise = Left bindings
 
+
 extendBindings :: ProgVal -> ProgVal -> Binding -> Either Binding String
 extendBindings (Variable y) x (Binding bindings)
                             | occursCheck (Variable y) x (Binding bindings) = Right "Extend bindings failed"
                             | otherwise = Left (Binding $ M.insert (Variable y) x bindings)
 extendBindings _ _ _ = Right "Inavlid binding format!"
+
 
 occursCheck :: ProgVal -> ProgVal -> Binding -> Bool
 occursCheck y x _
